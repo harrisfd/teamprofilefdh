64de218800c91b42b341fa55186bc9b8154b2bcf
ldylicia <fharrisfoster@gmail.com>
2021-04-17 16:07:43 -0700

    styling card proflies

--- src/generate.js
+++ src/generate.js
@@ -8,16 +8,30 @@ function generateHtml(employees) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team Profile</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-</head>
+    <link rel="stylesheet" type="text/css" href="../lib/style.css">
+    <link rel="stylesheet" type="text/css" href="../lib/style.css">
+    </head>
 <body>
-    <h1>My Team</h1>
+    <h1>Meet The Team</h1>
+    <div class="row">
+    <div class="container" id="team">
+    <div class="row">
+        <div class="col-12 text-center bg-info my-5 py-5 display-4 text-Black">Team Profile Generator</div>
+    </div>
+    <div class="row" id="cards">
+        
+
+    </div>
+
+
+</div>
     `;
 
     for (let i = 0; i < employees.length; i++) {
         const element = employees[i];
         switch (element.role) {
             case "Intern":
-                html += `<div class="card" style="width: 18rem;">
+                html += `<div class="card text-dark bg-info mb-3" style="width: 18rem;">
 <h2>${element.name}</h2>
 <h3>${element.role}</h3>
 <img src="../Images/Intern.jpg" class="card-img-top" alt="...">
@@ -29,7 +43,7 @@ function generateHtml(employees) {
 </div>`
                 break;
             case "Manager":
-                html += `<div class="card" style="width: 18rem;">
+                html += `<div class="card text-dark bg-info mb-3" style="width: 18rem;">
                 <h2>${element.name}</h2>
                 <h3>${element.role}</h3>
                 <img src="../Images/Manager.jpg" class="card-img-top" alt="...">
@@ -41,7 +55,7 @@ function generateHtml(employees) {
                 </div>`
                 break;
             case "Engineer":
-                html += `<div class="card" style="width: 18rem;">
+                html += `<div class="card text-dark bg-info mb-3" style="width: 18rem;">
                 <h2>${element.name}</h2>
                 <h3>${element.role}</h3>
                 <img src="../Images/Engineer.jpg" class="card-img-top" alt="...">
@@ -57,7 +71,8 @@ function generateHtml(employees) {
         }
 
     }
-    html += `</body>
+    html += `</div>
+    </body>
     </html>`
     return html
 }
